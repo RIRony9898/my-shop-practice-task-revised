25cde4e84da4854cec962c9fab33fec4a400e5ea
Md Raisul Islam Rony <rirony59@gmail.com>
2025-01-26 15:20:18 +0600

    `feat: updated Bottle and Bottles components with new styles and functionality`

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,16 +1,24 @@
 import './Bottle.css'
+import PropTypes from 'prop-types'
 
-const Bottle = ({bottle}) => {
+
+const Bottle = ({bottle,handlePurchase}) => {
     const {name, price, img} = bottle;
   return (
-    <div>
+    <div className="bottle">
         <div>
         <img src={img} alt={name}/>
         </div>
         <h3>{name}</h3>
         <p>{price}</p>
+        <button onClick={() => handlePurchase(bottle)}>Purchase</button>
     </div>
   )
+}
+
+Bottle.propTypes = {
+    bottle:PropTypes.object.isRequired,
+    handlePurchase:PropTypes.func.isRequired
 }
 
 export default Bottle
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,27 +1,44 @@
-import { useState, useEffect } from 'react';
-import Bottle from '../Bottle/Bottle';
-import './Bottles.css';
+import { useState, useEffect } from "react";
+import Bottle from "../Bottle/Bottle";
+import "./Bottles.css";
+import PropTypes from 'prop-types'
+
 
 const Bottles = () => {
-    const [bottles, setBottles] = useState([]);
+  const [bottles, setBottles] = useState([]);
+  const [cart, setCart] = useState([]);
 
-    useEffect(() => {
-        fetch('/bottles.json')
-            .then(res => res.json())
-            .then(data => setBottles(data))
-            .catch(err => console.error('Error fetching data:', err));
-    }, []);
+  useEffect(() => {
+    fetch("/bottles.json")
+      .then((res) => res.json())
+      .then((data) => setBottles(data))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
 
-    return (
-        <div>
-            <h2>Bottles: {bottles.length}</h2>
-            <div className="bottles-container">
-                {
-                    bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
-                }
-            </div>
-        </div>
-    );
-}
+  const handlePurchase = (bottle) => {
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+  };
+
+  return (
+    <div>
+      <h2>Available Bottles: {bottles.length}</h2>
+      <h3>Cart: {cart.length}</h3>
+      <div className="bottles-container">
+        {bottles.map((bottle) => (
+          <Bottle
+            key={bottle.id}
+            bottle={bottle}
+            handlePurchase={handlePurchase}
+          ></Bottle>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+Bottles.propTypes = {
+  bottles: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Bottles;
